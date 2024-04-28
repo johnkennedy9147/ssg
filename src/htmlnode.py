334bd9e89f6234ccf3dffad2760fc24687dcccda
John Kennedy <jk@DESKTOP-CKMGIOD>
2024-04-28 10:43:37 +0100

    update repr in parentnode and htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,4 +17,4 @@ class HTMLNode:
     return props_html
 
   def __repr__(self):
-    return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props}'
+    return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props}'
